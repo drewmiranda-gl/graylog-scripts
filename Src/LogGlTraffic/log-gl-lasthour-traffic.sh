155b7e04f0b1b62536886d2c86263471a9b9fa6b
Drew Miranda <drew.miranda@graylog.com>
2022-08-23 15:35:22 -0500

    log date, hour, output to log file

--- Src/LogGlTraffic/log-gl-lasthour-traffic.sh
+++ Src/LogGlTraffic/log-gl-lasthour-traffic.sh
@@ -1,3 +1,7 @@
+curdt=$(date +"%Y-%m-%d")
 mongjs=$(mongo graylog --eval 'db.traffic.find({}).limit(2).sort({bucket:-1})' --quiet | tail -n 1)
 numonly=$(echo $mongjs | grep -oP "\"output\"\s?:\s?{\s?.*?NumberLong\(\d+\)" | grep -oP "\(\d+\)" | grep -oP "\d+")
-curl -X POST -H 'Content-Type: application/json' -d "{ \"version\": \"1.1\", \"host\": \"example.org\", \"short_message\": \"A short message\", \"level\": 5, \"_some_info\": \"foo\", \"mongo_json\":\"$numonly\" }" 'http://graylog.drew.local:12201/gelf'
+dateonly=$(echo $mongjs | grep -oP "ISODate\((.*?)\)" | grep -oP "\d{4}-\d{2}-\d{2}")
+utc_hour_only=$(echo $mongjs | grep -oP "ISODate\((.*?)\)" | grep -oP "\d{4}-\d{2}-\d{2}T\d{2}" | grep -oP "T\d{2}" | grep -oP "\d+")
+# curl -X POST -H 'Content-Type: application/json' -d "{ \"host\": \"graylog_traffic\", \"short_message\": \"graylog traffic\", \"mongo_output_traffic_bytes\":\"$numonly\", \"date_only\":\"$curdt\" }" 'http://graylog.drew.local:12201/gelf'
+echo "{ \"host\": \"graylog_traffic\", \"short_message\": \"graylog traffic\", \"mongo_output_traffic_bytes\":$numonly, \"date_only\":\"$dateonly\", \"utc_hour_only\":\"$utc_hour_only\" }" >> /var/log/graylog-traffic/traffic.log
