23d5e62c3af042b963d8b6fca2745d8e335a3ca8
Drew Miranda <drew.miranda@graylog.com>
2023-03-10 12:48:15 -0600

    fixes, boy howdy don't mix up sh and bash because they are NOT the same

--- Src/Dynamic-Public-Ip-Changed/install.sh
+++ Src/Dynamic-Public-Ip-Changed/install.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 sudo mkdir -p /opt/track_public_ip_changes
+
+# */1 * * * * /usr/bin/sh /opt/track_public_ip_changes/track_public_ip_changes.sh >> /opt/track_public_ip_changes/debug.log
--- Src/Dynamic-Public-Ip-Changed/track_public_ip_changes.sh
+++ Src/Dynamic-Public-Ip-Changed/track_public_ip_changes.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # track_public_ip_changes.sh
 
@@ -6,33 +7,51 @@ workingpath=/opt/track_public_ip_changes
 pastvaluefile=$workingpath/prev_ip
 logfile=$workingpath/log.log
 
+echo "workingpath=${workingpath}" >> $logfile
+echo "pastvaluefile=${pastvaluefile}" >> $logfile
+
 iFileExists=0
 iIpHasChanged=0
 
+which dig >> $logfile
+dig +short myip.opendns.com @resolver1.opendns.com >> $logfile
+
 if [ -f "$pastvaluefile" ]; then
     iFileExists=1
+    echo "prev_ip file exists." >> $logfile
+else
+    echo "prev_ip file DOES NOT exist." >> $logfile
 fi
 
-if [[ "$iFileExists" == "1" ]]; then
+echo "iFileExists = ${iFileExists}" >> $logfile
+
+publicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+if [ $iFileExists -eq 1 ]; then
     previp=$(cat $pastvaluefile | grep -oP "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
     echo "Previously Recorded Ip: ${previp}"
-    publicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+    echo "Previously Recorded Ip: ${previp}" >> $logfile
+    
     echo "Current Ip: $publicip"
+    echo "Current Ip: $publicip" >> $logfile
 
-    if [[ "$previp" != "$publicip" ]]; then
+    if [ "$previp" != "$publicip" ]; then
         echo "IP Address has changed!"
         iIpHasChanged=1
     fi
 else
+    echo "File does not already exist, writing current Public IP to prev_ip" >> $logfile
     echo "${publicip}" > $pastvaluefile
 fi
 
 
-if [[ "$iIpHasChanged" == "1" ]]; then
+if [ $iIpHasChanged -eq 1 ]; then
     now=$(date)
-    echo "${now} :: ip_changed=1, old_ip=${previp}, new_ip=${publicip}" >> logfile
+    echo "${now} :: ip_changed=1, old_ip=${previp}, new_ip=${publicip}" >> $logfile
     echo "${publicip}" > $pastvaluefile
 else
     now=$(date)
-    echo "${now} :: ip_changed=0, ip=${publicip}" >> logfile
+    echo "${now} :: ip_changed=0, ip=${publicip}" >> $logfile
 fi
+
+
